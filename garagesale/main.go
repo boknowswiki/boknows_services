9639b2aec1fb673668ef9400ae9f8bf3e2a75e3a
boknowswiki@gmail.com <taobo0626@gmail.com>
2020-12-20 22:03:16 -0800

    add json formant

--- garagesale/main.go
+++ garagesale/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
@@ -14,56 +13,90 @@ import (
 
 func main() {
 
-	log.Printf("main: started")
-	defer log.Println("main: finished")
+	// =========================================================================
+	// App Starting
+
+	log.Printf("main : Started")
+	defer log.Println("main : Completed")
+
+	// =========================================================================
+	// Start API Service
 
 	api := http.Server{
 		Addr:         "localhost:8000",
-		Handler:      http.HandlerFunc(Echo),
+		Handler:      http.HandlerFunc(ListProducts),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	}
 
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
 	serverErrors := make(chan error, 1)
 
+	// Start the service listening for requests.
 	go func() {
-		log.Printf("Starting to listen on %s", api.Addr)
+		log.Printf("main : API listening on %s", api.Addr)
 		serverErrors <- api.ListenAndServe()
 	}()
 
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
 
+	// =========================================================================
+	// Shutdown
+
+	// Blocking main and waiting for shutdown.
 	select {
 	case err := <-serverErrors:
 		log.Fatalf("error: listening and serving: %s", err)
-	case sig := <-shutdown:
-		log.Printf("main shuting down: %s", sig)
 
+	case <-shutdown:
+		log.Println("main : Start shutdown")
+
+		// Give outstanding requests a deadline for completion.
 		const timeout = 5 * time.Second
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
 
+		// Asking listener to shutdown and load shed.
 		err := api.Shutdown(ctx)
 		if err != nil {
-			log.Printf("main gracefull shutdown in %v: %v", timeout, err)
+			log.Printf("main : Graceful shutdown did not complete in %v : %v", timeout, err)
 			err = api.Close()
 		}
 
 		if err != nil {
-			log.Fatalf("main: could not shutdown gracefully: %v", err)
+			log.Fatalf("main : could not stop server gracefully : %v", err)
 		}
-
 	}
 }
 
-// Echo is a basic HTTP Handler.
-func Echo(w http.ResponseWriter, r *http.Request) {
-	id := rand.Intn(1000)
-	log.Printf("starting id: %v", id)
-	defer log.Printf("end id: %v", id)
+// Product is an item we sell.
+type Product struct {
+	Name     string `json:"name"`
+	Cost     int    `json:"cost"`
+	Quantity int    `json:"quantity"`
+}
 
-	time.Sleep(3 * time.Second)
+// ListProducts is an HTTP Handler for returning a list of Products.
+func ListProducts(w http.ResponseWriter, r *http.Request) {
+	list := []Product{
+		{Name: "Comic Books", Cost: 50, Quantity: 42},
+		{Name: "McDonalds Toys", Cost: 75, Quantity: 120},
+	}
 
-	fmt.Fprintf(w, "You asked to %s %s\n", r.Method, r.URL.Path)
+	data, err := json.Marshal(list)
+	if err != nil {
+		log.Println("error marshalling result", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		log.Println("error writing result", err)
+	}
 }
