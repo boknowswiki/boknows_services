6cf4a87a1f2266c9ff13b684fdc49fe327ce0142
boknowswiki@gmail.com <taobo0626@gmail.com>
2021-01-03 10:53:27 -0800

    Add user command.

--- garagesale/cmd/sales-admin/main.go
+++ garagesale/cmd/sales-admin/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/boknowswiki/boknows_services/garagesale/internal/platform/auth"
 	"github.com/boknowswiki/boknows_services/garagesale/internal/platform/conf"
 	"github.com/boknowswiki/boknows_services/garagesale/internal/platform/database"
 	"github.com/boknowswiki/boknows_services/garagesale/internal/schema"
+	"github.com/boknowswiki/boknows_services/garagesale/internal/user"
 	"github.com/pkg/errors"
 )
 
@@ -18,6 +22,7 @@ func main() {
 }
 
 func run() error {
+
 	// =========================================================================
 	// Configuration
 
@@ -36,46 +41,112 @@ func run() error {
 		if err == conf.ErrHelpWanted {
 			usage, err := conf.Usage("SALES", &cfg)
 			if err != nil {
-				return errors.Wrap(err, "main : generating usage")
+				return errors.Wrap(err, "generating usage")
 			}
 			fmt.Println(usage)
 			return nil
 		}
-		return errors.Wrap(err, "parsing config")
+		return errors.Wrap(err, "error: parsing config")
 	}
 
-	cfg.DB.DisableTLS = true
-
-	// Initialize dependencies.
-	db, err := database.Open(database.Config{
+	// This is used for multiple commands below.
+	dbConfig := database.Config{
 		User:       cfg.DB.User,
 		Password:   cfg.DB.Password,
 		Host:       cfg.DB.Host,
 		Name:       cfg.DB.Name,
 		DisableTLS: cfg.DB.DisableTLS,
-	})
+	}
+
+	dbConfig.DisableTLS = true
+
+	var err error
+	switch cfg.Args.Num(0) {
+	case "migrate":
+		err = migrate(dbConfig)
+	case "seed":
+		err = seed(dbConfig)
+	case "useradd":
+		err = useradd(dbConfig, cfg.Args.Num(1), cfg.Args.Num(2))
+	default:
+		err = errors.New("Must specify a command")
+	}
+
 	if err != nil {
-		return errors.Wrap(err, "connecting to db")
+		return err
+	}
+
+	return nil
+}
+
+func migrate(cfg database.Config) error {
+	db, err := database.Open(cfg)
+	if err != nil {
+		return err
 	}
 	defer db.Close()
 
-	switch cfg.Args.Num(0) {
-	case "migrate":
-		if err := schema.Migrate(db); err != nil {
-			log.Println("error applying migrations", err)
-			os.Exit(1)
-		}
-		fmt.Println("Migrations complete")
-		return nil
+	if err := schema.Migrate(db); err != nil {
+		return err
+	}
 
-	case "seed":
-		if err := schema.Seed(db); err != nil {
-			log.Println("error seeding database", err)
-			os.Exit(1)
-		}
-		fmt.Println("Seed data complete")
+	fmt.Println("Migrations complete")
+	return nil
+}
+
+func seed(cfg database.Config) error {
+	db, err := database.Open(cfg)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	if err := schema.Seed(db); err != nil {
+		return err
+	}
+
+	fmt.Println("Seed data complete")
+	return nil
+}
+
+func useradd(cfg database.Config, email, password string) error {
+	db, err := database.Open(cfg)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	if email == "" || password == "" {
+		return errors.New("useradd command must be called with two additional arguments for email and password")
+	}
+
+	fmt.Printf("Admin user will be created with email %q and password %q\n", email, password)
+	fmt.Print("Continue? (1/0) ")
+
+	var confirm bool
+	if _, err := fmt.Scanf("%t\n", &confirm); err != nil {
+		return errors.Wrap(err, "processing response")
+	}
+
+	if !confirm {
+		fmt.Println("Canceling")
 		return nil
 	}
 
+	ctx := context.Background()
+
+	nu := user.NewUser{
+		Email:           email,
+		Password:        password,
+		PasswordConfirm: password,
+		Roles:           []string{auth.RoleAdmin, auth.RoleUser},
+	}
+
+	u, err := user.Create(ctx, db, nu, time.Now())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("User created with id:", u.ID)
 	return nil
 }
