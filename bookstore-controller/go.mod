e1cf34e4d5be3f17ae1e73505c039e2bfe97b506
Bo Tao <boknowswiki@gmail.com>
2021-01-25 17:07:42 -0800

    bookstore works as samplecontroller now.

--- bookstore-controller/go.mod
+++ bookstore-controller/go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
-	k8s.io/api v0.0.0-20210115125903-c873f2e8ab25 // indirect
+	k8s.io/api v0.0.0-20210115125903-c873f2e8ab25
 	k8s.io/apimachinery v0.0.0-20210116005712-af2ce7e24233
 	k8s.io/client-go v0.0.0-20210114130407-537eda74d850
 	k8s.io/code-generator v0.20.2
--- bookstore-controller/main.go
+++ bookstore-controller/main.go
@@ -1,66 +1,73 @@
 package main
 
 import (
-    "log"
-    "flag"
-    "time"
+	"flag"
+	"log"
+	"time"
 
-    kubeinformers "k8s.io/client-go/informers"
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/tools/clientcmd"
-    "k8s.io/klog/v2"
-    // Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
-    // _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-       
-    clientset "github.com/boknowswiki/boknows_services/bookstore-controller/pkg/generated/clientset/versioned"
-    informers "github.com/boknowswiki/boknows_services/bookstore-controller/pkg/generated/informers/externalversions"
-    "github.com/boknowswiki/boknows_services/bookstore-controller/pkg/signals"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+
+	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
+	"github.com/boknowswiki/boknows_services/bookstore-controller/controller"
+	clientset "github.com/boknowswiki/boknows_services/bookstore-controller/pkg/generated/clientset/versioned"
+	informers "github.com/boknowswiki/boknows_services/bookstore-controller/pkg/generated/informers/externalversions"
+	"github.com/boknowswiki/boknows_services/bookstore-controller/pkg/signals"
 )
 
 var (
-    masterURL  string
-    kubeconfig string
+	masterURL  string
+	kubeconfig string
 )
 
-func main () {
-    klog.InitFlags(nil)
-    flag.Parse()
-    log.Println("Hello main in bookstore-controller!")
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+	log.Println("Hello main in bookstore-controller!")
 
-   // set up signals so we handle the first shutdown signal gracefully
-    stopCh := signals.SetupSignalHandler()
-    _ = stopCh
-   
-    cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
-    if err != nil {
-        klog.Fatalf("Error building kubeconfig: %s", err.Error())
-    }
-   
-    kubeClient, err := kubernetes.NewForConfig(cfg)
-    if err != nil {
-        klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
-    }
-   
-    exampleClient, err := clientset.NewForConfig(cfg)
-    if err != nil {
-        klog.Fatalf("Error building example clientset: %s", err.Error())
-    }
-   
-    kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
-    exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+	_ = stopCh
 
-    _, _ = kubeInformerFactory, exampleInformerFactory
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
 
-    //controller := NewController(kubeClient, exampleClient,
-    //    kubeInformerFactory.Apps().V1().Deployments(),
-    //    exampleInformerFactory.Samplecontroller().V1alpha1().Foos())
-    //         
-    //// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
-    //// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
-    //kubeInformerFactory.Start(stopCh)
-    //exampleInformerFactory.Start(stopCh)
-    //         
-    //if err = controller.Run(2, stopCh); err != nil {
-    //    klog.Fatalf("Error running controller: %s", err.Error())
-    //} 
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	exampleClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building example clientset: %s", err.Error())
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+
+	_, _ = kubeInformerFactory, exampleInformerFactory
+
+	controller := controller.NewController(kubeClient, exampleClient,
+		kubeInformerFactory.Apps().V1().Deployments(),
+		exampleInformerFactory.Bookcontroller().V1().Books())
+
+	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
+	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+	kubeInformerFactory.Start(stopCh)
+	exampleInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
+}
+
+func init() {
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 }
